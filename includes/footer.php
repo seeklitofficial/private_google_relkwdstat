eee71b0957b39abd0d693f604cfa3c04ceeee7b9
skymelod <seeklitofficial@gmail.com>
2025-10-05 05:00:14 +0000

    private_html 구조 정리
    - private_html에서 불필요한 파일들 제거
    - index.php 제거 (public_html에만 있어야 함)
    - includes 폴더 제거 (public_html에만 있어야 함)
    - private_html은 보안상 외부 접근 불가능한 폴더
    - 올바른 웹 서버 구조로 정리 완료

--- includes/footer.php
+++
@@ -1,4 +0,0 @@
-    </div>
-    <script src="/google_blog_analyzer/google_blog_analyzer_script.js"></script>
-</body>
-</html>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/header.php';
-require_once __DIR__ . '/google_blog_analyzer/google_blog_analyzer_content.php';
-require_once __DIR__ . '/includes/footer.php';
